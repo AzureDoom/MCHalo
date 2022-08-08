c72c80af5b68eb9fc437e1b05f352d29d33f3825
AzureZhen <rcraft@cyberconnects2.com>
2022-08-08 17:09:08 -0400

    v1.1.2

--- src/main/java/mod/azure/mchalo/util/HaloSounds.java
+++ src/main/java/mod/azure/mchalo/util/HaloSounds.java
@@ -6,27 +6,33 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class HaloSounds {
-	public static SoundEvent SNIPER = of("mchalo.sniper_fire_h3");
-	public static SoundEvent SNIPERRELOAD = of("mchalo.sniper_reload");
-	public static SoundEvent SHOTGUN = of("mchalo.shotgun");
-	public static SoundEvent SHOTGUNRELOAD = of("mchalo.shotgunreload");
-	public static SoundEvent PISTOL = of("mchalo.pistol");
-	public static SoundEvent PISTOLRELOAD = of("mchalo.pistolreload");
-	public static SoundEvent BATTLERIFLE = of("mchalo.battle_rifle");
-	public static SoundEvent BATTLERIFLERELOAD = of("mchalo.battle_rifle_reload");
-	public static SoundEvent ROCKET = of("mchalo.rocket_h3_1");
-	public static SoundEvent ROCKETRELOAD = of("mchalo.rocket_reload");
+	public static SoundEvent SNIPER = of("sniper_fire_h3");
+	public static SoundEvent SNIPERRELOAD = of("sniper_reload");
+	public static SoundEvent SHOTGUN = of("shotgun");
+	public static SoundEvent SHOTGUNRELOAD = of("shotgunreload");
+	public static SoundEvent PISTOL = of("pistol");
+	public static SoundEvent PISTOLRELOAD = of("pistolreload");
+	public static SoundEvent BATTLERIFLE = of("battle_rifle");
+	public static SoundEvent BATTLERIFLERELOAD = of("battle_rifle_reload");
+	public static SoundEvent ROCKET = of("rocket_h3_1");
+	public static SoundEvent ROCKETRELOAD = of("rocket_reload");
 
-	public static SoundEvent PLASMAPISTOL = of("mchalo.plasmapistol");
-	public static SoundEvent PLASMAPISTOLELOAD = of("mchalo.plasmapistol_reload");
-	public static SoundEvent PLASMARIFLE = of("mchalo.plasmarifle");
-	public static SoundEvent PLASMARIFLERELOAD = of("mchalo.plasmarifle_reload");
-	public static SoundEvent NEEDLER = of("mchalo.needler");
-	public static SoundEvent NEEDLERRELOAD = of("mchalo.needler_reload");
-	public static SoundEvent MAULER = of("mchalo.mauler");
-	public static SoundEvent MAULERRELOAD = of("mchalo.mauler_reload");
-	public static SoundEvent BRUTESHOT = of("mchalo.bruteshot");
-	public static SoundEvent BRUTESHOTRELOAD = of("mchalo.bruteshot_reload");
+	public static SoundEvent PLASMAPISTOL = of("plasmapistol");
+	public static SoundEvent PLASMAPISTOLELOAD = of("plasmapistol_reload");
+	public static SoundEvent PLASMARIFLE = of("plasmarifle");
+	public static SoundEvent PLASMARIFLERELOAD = of("plasmarifle_reload");
+	public static SoundEvent NEEDLER = of("needler");
+	public static SoundEvent NEEDLERRELOAD = of("needler_reload");
+	public static SoundEvent MAULER = of("mauler");
+	public static SoundEvent MAULERRELOAD = of("mauler_reload");
+	public static SoundEvent BRUTESHOT = of("bruteshot");
+	public static SoundEvent BRUTESHOTRELOAD = of("bruteshot_reload");
+	public static SoundEvent ENERGYSWORD1A = of("energy_melee1a");
+	public static SoundEvent ENERGYSWORD1B = of("energy_melee1b");
+	public static SoundEvent ENERGYSWORD1C = of("energy_melee1c");
+	public static SoundEvent BRUTESHOT_MELEE1 = of("brute_shot_melee1");
+	public static SoundEvent BRUTESHOT_MELEE2 = of("brute_shot_melee2");
+	public static SoundEvent BRUTESHOT_MELEE3 = of("brute_shot_melee3");
 
 	static SoundEvent of(String id) {
 		SoundEvent sound = new SoundEvent(new Identifier(MCHaloMod.MODID, id));
