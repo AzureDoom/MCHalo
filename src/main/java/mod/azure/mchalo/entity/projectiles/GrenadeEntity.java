5ba14adb7f208fc0a0673c255c73b4f103368088
AzureZhen <7415711+AzureDoom@users.noreply.github.com>
2022-09-17 11:53:20 -0400

    v1.1.8

--- src/main/java/mod/azure/mchalo/entity/projectiles/GrenadeEntity.java
+++ src/main/java/mod/azure/mchalo/entity/projectiles/GrenadeEntity.java
@@ -1,14 +1,11 @@
 package mod.azure.mchalo.entity.projectiles;
 
-import java.util.Iterator;
-
 import mod.azure.mchalo.config.HaloConfig;
 import mod.azure.mchalo.network.HaloEntityPacket;
 import mod.azure.mchalo.util.HaloItems;
 import mod.azure.mchalo.util.ProjectilesEntityRegister;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.minecraft.block.BlockState;
 import net.minecraft.entity.AreaEffectCloudEntity;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
@@ -16,9 +13,9 @@ import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.data.DataTracker;
 import net.minecraft.entity.data.TrackedData;
 import net.minecraft.entity.data.TrackedDataHandlerRegistry;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.projectile.PersistentProjectileEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.network.Packet;
 import net.minecraft.particle.ParticleTypes;
@@ -26,13 +23,6 @@ import net.minecraft.sound.SoundEvent;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.hit.EntityHitResult;
-import net.minecraft.util.hit.HitResult;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Box;
-import net.minecraft.util.math.MathHelper;
-import net.minecraft.util.math.Vec3d;
-import net.minecraft.util.shape.VoxelShape;
-import net.minecraft.world.RaycastContext;
 import net.minecraft.world.World;
 import net.minecraft.world.explosion.Explosion;
 import software.bernie.geckolib3.core.IAnimatable;
@@ -68,10 +58,7 @@ public class GrenadeEntity extends PersistentProjectileEntity implements IAnimat
 	protected GrenadeEntity(EntityType<? extends GrenadeEntity> type, LivingEntity owner, World world) {
 		this(type, owner.getX(), owner.getEyeY() - 0.10000000149011612D, owner.getZ(), world);
 		this.setOwner(owner);
-		if (owner instanceof PlayerEntity) {
-			this.pickupType = PersistentProjectileEntity.PickupPermission.ALLOWED;
-		}
-
+		this.pickupType = PersistentProjectileEntity.PickupPermission.DISALLOWED;
 	}
 
 	public GrenadeEntity(World world, LivingEntity user, boolean spinning) {
@@ -155,97 +142,6 @@ public class GrenadeEntity extends PersistentProjectileEntity implements IAnimat
 	@Override
 	public void tick() {
 		super.tick();
-		boolean bl = this.isNoClip();
-		BlockPos blockPos = this.getBlockPos();
-		BlockState blockState = this.world.getBlockState(blockPos);
-		Vec3d vec3d = this.getVelocity();
-		Vec3d vec3d4;
-		if (!blockState.isAir() && !bl) {
-			VoxelShape voxelShape = blockState.getCollisionShape(this.world, blockPos);
-			if (!voxelShape.isEmpty()) {
-				vec3d4 = this.getPos();
-				Iterator<?> var7 = voxelShape.getBoundingBoxes().iterator();
-
-				while (var7.hasNext()) {
-					Box box = (Box) var7.next();
-					if (box.offset(blockPos).contains(vec3d4)) {
-						this.dataTracker.set(SPINNING, false);
-						this.inGround = true;
-						break;
-					}
-				}
-				this.setSpinning(false);
-			}
-		}
-		if (this.prevPitch == 0.0F && this.prevYaw == 0.0F) {
-			double f = vec3d.horizontalLength();
-			this.setYaw((float) (MathHelper.atan2(vec3d.x, vec3d.z) * 57.2957763671875D));
-			this.setPitch((float) (MathHelper.atan2(vec3d.y, f) * 57.2957763671875D));
-			this.prevYaw = this.getYaw();
-			this.prevPitch = this.getPitch();
-		}
-		if (this.age >= 80) {
-			this.remove(Entity.RemovalReason.DISCARDED);
-		}
-		if (this.inAir && !bl) {
-			this.age();
-			++this.timeInAir;
-		} else {
-			this.timeInAir = 0;
-			Vec3d vec3d3 = this.getPos();
-			Vec3d vector3d3 = vec3d3.add(vec3d);
-			HitResult hitResult = this.world.raycast(new RaycastContext(vec3d3, vector3d3,
-					RaycastContext.ShapeType.COLLIDER, RaycastContext.FluidHandling.NONE, this));
-			if (((HitResult) hitResult).getType() != HitResult.Type.MISS) {
-				vector3d3 = ((HitResult) hitResult).getPos();
-			}
-			while (!this.isRemoved()) {
-				EntityHitResult entityHitResult = this.getEntityCollision(vec3d3, vector3d3);
-				if (entityHitResult != null) {
-					hitResult = entityHitResult;
-				}
-				if (hitResult != null && ((HitResult) hitResult).getType() == HitResult.Type.ENTITY) {
-					Entity entity = ((EntityHitResult) hitResult).getEntity();
-					Entity entity2 = this.getOwner();
-					if (entity instanceof PlayerEntity && entity2 instanceof PlayerEntity
-							&& !((PlayerEntity) entity2).shouldDamagePlayer((PlayerEntity) entity)) {
-						hitResult = null;
-						entityHitResult = null;
-					}
-				}
-				if (hitResult != null && !bl) {
-					this.onCollision((HitResult) hitResult);
-					this.velocityDirty = true;
-				}
-				if (entityHitResult == null || this.getPierceLevel() <= 0) {
-					break;
-				}
-				hitResult = null;
-			}
-			vec3d = this.getVelocity();
-			double d = vec3d.x;
-			double e = vec3d.y;
-			double g = vec3d.z;
-			double h = this.getX() + d;
-			double j = this.getY() + e;
-			double k = this.getZ() + g;
-			double l = vec3d.horizontalLength();
-			if (bl) {
-				this.setYaw((float) (MathHelper.atan2(-e, -g) * 57.2957763671875D));
-			} else {
-				this.setYaw((float) (MathHelper.atan2(e, g) * 57.2957763671875D));
-			}
-			this.setPitch((float) (MathHelper.atan2(e, l) * 57.2957763671875D));
-			this.setPitch(updateRotation(this.prevPitch, this.getPitch()));
-			this.setYaw(updateRotation(this.prevYaw, this.getYaw()));
-			float m = 0.99F;
-
-			this.setVelocity(vec3d.multiply((double) m));
-			Vec3d vec3d5 = this.getVelocity();
-			this.setVelocity(vec3d5.x, vec3d5.y - 0.05000000074505806D, vec3d5.z);
-			this.updatePosition(h, j, k);
-			this.checkBlockCollision();
-		}
 	}
 
 	public void initFromStack(ItemStack stack) {
@@ -292,14 +188,14 @@ public class GrenadeEntity extends PersistentProjectileEntity implements IAnimat
 	}
 
 	protected void explode() {
-		this.world.createExplosion(this, this.getX(), this.getBodyY(0.0625D), this.getZ(), HaloConfig.mauler_bullet_damage,
-				(HaloConfig.grenades_cause_fire ? true : false),
+		this.world.createExplosion(this, this.getX(), this.getBodyY(0.0625D), this.getZ(),
+				HaloConfig.mauler_bullet_damage, (HaloConfig.grenades_cause_fire ? true : false),
 				(HaloConfig.grenades_break_blocks ? Explosion.DestructionType.BREAK : Explosion.DestructionType.NONE));
 	}
 
 	@Override
 	public ItemStack asItemStack() {
-		return new ItemStack(HaloItems.GRENADE);
+		return new ItemStack(Items.AIR);
 	}
 
 	@Override
